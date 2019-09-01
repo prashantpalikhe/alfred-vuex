df90f9b2989a3f64ce13f13e102ac5390fc973a9
Prashant Palikhe <prashpal09@gmail.com>
2019-09-01 11:25:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const alfy = require('alfy');
+const algoliasearch = require('algoliasearch');
+const { last } = require('lodash');
+
+const client = algoliasearch(
+	'BH4D9OD16A',
+	'97f135e4b5f5487fb53f0f2dae8db59d'
+);
+const index = client.initIndex('vuex');
+
+(async () => {
+	const { hits } = await index.search({
+		query: alfy.input,
+		hitsPerPage: 5
+	});
+
+	const output = hits.map(hit => {
+		const result = {
+			title: hit.anchor,
+			subtitle: hit.anchor,
+			arg: hit.url,
+			quicklookurl: hit.url
+		};
+
+		if (hit.hierarchy) {
+			const hierarchies = Object.values(hit.hierarchy).filter(Boolean);
+
+			result.title = last(hierarchies);
+			result.subtitle = hierarchies.join(' > ');
+		}
+
+		return result;
+	});
+
+	alfy.output(output);
+})();
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-vuex [![Build Status](https://travis-ci.org/prashantpalikhe/alfred-vuex.svg?branch=master)](https://travis-ci.org/prashantpalikhe/alfred-vuex)
+
+> Alfred workflow for Vuex documentation
+
+
+## Install
+
+```
+$ npm install --global alfred-vuex
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `vuex`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Prashant Palikhe](https://twitter.com/prashantPalikhe)
